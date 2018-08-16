326d552c0512f0ae1de15fc785c0fffd14d4b620
FloLeh <florianlehuede@gmail.com>
2018-08-16 20:44:20 +0200

    puss dégueu

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  \
+
   root to: 'emailapi#index'
   post 'emailapi/subscribe', to: 'emailapi#subscribe'
-  get 'xXnicobgdu44Xx', to: 'landing_pages#index', as: "xXnicobgdu44Xx"
-  get '/index2', to: 'landing_pages#index2'
-  get 'index3', to:'landing_pages#index3'
+  get 'index', to: 'landing_pages#index'
+  get 'index2', to: 'landing_pages2#index'
+  get 'index3', to: 'landing_pages3#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
